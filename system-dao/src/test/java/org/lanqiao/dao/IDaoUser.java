e341b88be2137fd0683c63e2f3bbb63182414343
ld <ld@lanqiao.org>
2018-11-30 16:19:51 +0800

    新增分支提交内容

---
+++ system-dao/src/test/java/org/lanqiao/dao/IDaoUser.java
@@ -0,0 +1,7 @@
+package org.lanqiao.dao;
+
+public class IDaoUser {
+    public void daoTest(){
+        System.out.println("新增分支添加内容");
+    }
+}
--- system-dao/src/test/java/org/lanqiao/dao/domainTest.java
+++ system-dao/src/test/java/org/lanqiao/dao/domainTest.java
@@ -1,10 +1,15 @@
 package org.lanqiao.dao;
 
 
+import org.junit.Test;
+import org.lanqiao.User;
 
 public class domainTest {
+    @Test
     public  void UserTest(){
-
+        User user =new User();
+        user.showInfo("张三",16);
     }
+
 
 }
